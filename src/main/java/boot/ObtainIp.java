15076ed5b7852e4002fcbb6fb469976a32333191
DotDashDotDash <1223362570@qq.com>
2018-12-19 20:36:40 +0800

    文件结构

--- src/main/java/boot/ObtainIp.java
+++ src/main/java/boot/ObtainIp.java
@@ -6,7 +6,7 @@ import util.IpFetcher.KuaiDailiFetcher;
 
 public class ObtainIp {
     public static void main(String[] args) {
-        KuaiDailiFetcher kuaiDailiFetcher = new KuaiDailiFetcher(5);
+        KuaiDailiFetcher kuaiDailiFetcher = new KuaiDailiFetcher(10);
         SQLServerUtil util = new SQLServerUtil();
         try {
             util.clearTableWithTableName("VariFlight", "CrawledIp");
